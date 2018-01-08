087221c3f8122a47f8f1a7088be308e0b3efd94b
Dmitry Sky <dmitry.sky@icloud.com>
2018-01-08 16:03:39 -0700

    establish a connection to Pusher, subscribe to coin prices, and push(pun intended) prices to clients.

--- src/Today/Today.js
+++ src/Today/Today.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Today.css'
 import axios from 'axios'
+import Pusher from 'pusher-js'
 
 class Today extends Component {
   // Adds a class constructor that assigns the initial state values:
@@ -14,6 +15,14 @@ class Today extends Component {
   }
   // This is called when an instance of a component is being created and inserted into the DOM.
   componentWillMount () {
+      // establish a connection to Pusher
+      this.pusher = new Pusher('APP_KEY', {
+          cluster: 'YOUR_CLUSTER',
+          encrypted: true
+      });
+      // Subscribe to the 'coin-prices' channel
+      this.prices = this.pusher.subscribe('coin-prices');
+
       axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
           .then(response => {
               // We set the latest prices in the state to the prices gotten from Cryptocurrency.
@@ -26,6 +35,37 @@ class Today extends Component {
               console.log(error)
           })
   }
+
+  componentDidMount () {
+      setInterval(() => {
+          axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
+              .then(response => {
+                  this.sendPricePusher (response.data)
+              })
+              .catch(error => {
+                  console.log(error)
+              })
+      }, 10000)
+      // We bind to the 'prices' event and use the data in it (price information) to update the state values, thus, realtime changes
+      this.prices.bind('prices', price => {
+          this.setState({ btcprice: price.prices.BTC.USD });
+          this.setState({ ethprice: price.prices.ETH.USD });
+          this.setState({ ltcprice: price.prices.LTC.USD });
+      }, this);
+  }
+
+  sendPricePusher (data) {
+      axios.post('/prices/new', {
+          prices: data
+      })
+          .then(response => {
+              console.log(response)
+          })
+          .catch(error => {
+              console.log(error)
+          })
+  }
+
   // The render method contains the JSX code which will be compiled to HTML.
   render() {
       return (
